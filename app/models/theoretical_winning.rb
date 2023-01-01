858ebbb90d2cc467aa0fb9962b92ac585e4ca733
tokiniwa <tokiniwa@timegardens.com>
2023-01-02 00:52:53 +0900

    不要なモデルなどの削除

--- app/models/theoretical_winning.rb
+++
@@ -1,7 +0,0 @@
-class TheoreticalWinning < ApplicationRecord
-  validates :grade, presence: true,
-                    numericality: { only_integer: true, greater_than: 0, less_than: 6 },
-                    uniqueness: true
-  validates :winning, presence: true,
-                      numericality: { only_integer: true, greater_than: 0 }
-end
--- spec/factories/three_in_six_anticipations.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :three_in_six_anticipation do
-    association :event, strategy: :build
-  end
-end
