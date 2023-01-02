f779f77ef99a97686aeb064552b2274a1aefec01
tokiniwa <tokiniwa@timegardens.com>
2023-01-02 11:50:06 +0900

    データの微調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require 'csv'
 
-target_files = Dir.glob('db/data/loto_six.rb')
+# ロト6のデータ作成
+target_files = Dir.glob('db/data/loto_six.csv')
 
 ActiveRecord::Base.connection.execute("TRUNCATE TABLE loto_six_events;")
 ActiveRecord::Base.connection.execute("TRUNCATE TABLE loto_six_numbers;")
@@ -11,3 +12,17 @@ target_files.each do |file|
     1.upto(6) { |i| LotoSixNumber.create!(loto_six_event: event, lottery_number: row["number#{i}"].to_i, priority: i) }
   end
 end
+
+# ロト7のデータ作成
+target_files = Dir.glob('db/data/loto_seven.csv')
+
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE loto_six_events;")
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE loto_six_numbers;")
+
+target_files.each do |file|
+  CSV.foreach(file, headers: true) do |row|
+    event = LotoSixEvent.create!(lottery_id: row['lottery_id'].to_i, lottery_date: row['lottery_date'].to_date)
+    1.upto(6) { |i| LotoSixNumber.create!(loto_six_event: event, lottery_number: row["number#{i}"].to_i, priority: i) }
+  end
+end
+
